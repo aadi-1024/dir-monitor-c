ca7a7881494fedb67575aef8791356513d0c8656
Aaditya Thakur <athakur4_be22@thapar.edu>
2023-03-30 18:40:22 +0530

    Initial

---
+++ README.md
@@ -0,0 +1,9 @@
+## Automatically install RPMs or flatpaks that you copy into a particular directory
+
+Not complete yet, but if run with sudo, it will(once complete) detect any flatpakrefs or rpms being copied into the directory and install them. Runs silently in the background with next to no resources.
+
+Todo:
+- Complete it
+- Integrate with polkit
+- Add systemd service or smth
+- h
---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/inotify.h>
+#include <malloc.h>
+#include <string.h>
+#define PATH_MAX 4096
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        printf("Enter path to monitor\n");
+        return 1;
+    }
+    int fd = inotify_init();
+    if (fd == -1) {
+        printf("File descriptor could not be created");
+        return 1;
+    }
+    int wd = inotify_add_watch(fd, argv[1], IN_CREATE);
+    size_t bufsiz = sizeof(struct inotify_event) + PATH_MAX + 1;
+    struct inotify_event* event = malloc(bufsiz);
+    char* name;
+    while (1) {
+        read(fd, event, bufsiz);
+        name = event->name;
+        printf("%s\n", name);
+    }
+}
