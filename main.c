0f66d343fb93e4028baf369caff32282f7b65133
Aaditya Thakur <athakur4_be22@thapar.edu>
2023-04-20 17:31:51 +0530

    Installs flatpaks

--- main.c
+++ main.c
@@ -3,25 +3,35 @@
 #include <sys/inotify.h>
 #include <malloc.h>
 #include <string.h>
+#include <stdlib.h>
 #define PATH_MAX 4096
 
 int main(int argc, char** argv) {
-    if (argc < 2) {
-        printf("Enter path to monitor\n");
-        return 1;
-    }
+    //if (argc < 2) {
+    //    printf("Enter path to monitor\n");
+    //    return 1;
+    //}
+
     int fd = inotify_init();
     if (fd == -1) {
         printf("File descriptor could not be created");
         return 1;
     }
-    int wd = inotify_add_watch(fd, argv[1], IN_CREATE);
+    int wd = inotify_add_watch(fd, "/home/aaditya/Apps", IN_CREATE);
     size_t bufsiz = sizeof(struct inotify_event) + PATH_MAX + 1;
     struct inotify_event* event = malloc(bufsiz);
     char* name;
+    char* cmd = malloc(PATH_MAX);
+    strcpy(cmd, "flatpak install -y ~/Apps/");
     while (1) {
         read(fd, event, bufsiz);
         name = event->name;
-        printf("%s\n", name);
+	strcat(cmd, name);
+    strcat(cmd, " > /dev/null");
+
+	system(cmd);
+    
+    strcpy(cmd, "");
+    printf("Done! Installed %s\n", name);
     }
 }
